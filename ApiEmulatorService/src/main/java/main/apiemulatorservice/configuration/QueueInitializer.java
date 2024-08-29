79cd060d0e0e30b08dea696ead90ad3abd02d2a9
“Sparky775277” <sasha775275@mail.ru>
2024-08-29 17:09:06 +0300

    small changes

---
+++ ApiEmulatorService/src/main/java/main/apiemulatorservice/configuration/QueueInitializer.java
@@ -0,0 +1,23 @@
+package main.apiemulatorservice.configuration;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QueueInitializer {
+
+    private final RabbitAdmin rabbitAdmin;
+    private final Queue queue;
+
+    public QueueInitializer(RabbitAdmin rabbitAdmin, Queue queue) {
+        this.rabbitAdmin = rabbitAdmin;
+        this.queue = queue;
+    }
+
+    @PostConstruct
+    public void declareQueue() {
+        rabbitAdmin.declareQueue(queue);
+    }
+}
--- AuditoriumUpdateService/src/main/java/main/auditoriumupdateservice/AuditoriumUpdateServiceApplication.java
+++ AuditoriumUpdateService/src/main/java/main/auditoriumupdateservice/AuditoriumUpdateServiceApplication.java
@@ -1,9 +1,5 @@
 package main.auditoriumupdateservice;
 
-import jakarta.annotation.PostConstruct;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.core.RabbitAdmin;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
